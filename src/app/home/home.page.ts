daad6e1727343863c7dea5d234d96e5c85b3e74c
Marcos-Andr4de <kinzinpc@gmail.com>
2024-05-14 19:13:31 -0300

    add de informacoes do clima

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { environment } from './../../environments/environment';
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 const API_URL = environment.API_URL;
 const API_KEY = environment.API_KEY;
 @Component({
@@ -10,14 +11,26 @@ const API_KEY = environment.API_KEY;
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  weatherTemp :any
+  todayDate = new Date()
+  cityName :any
+  weatherIcon :any
+  weatherDetails:any
 
   constructor(public httpClient:HttpClient) {
     this.loadData()
   }
 
   loadData(){
-    this.httpClient.get(`${API_URL}/weather?q=${"Johannesburg"}&appid=${API_KEY}`).subscribe(results =>{
+    this.httpClient.get(`${API_URL}/weather?q=${"Rio de Janeiro"}&appid=${API_KEY}`).subscribe((results: any) =>{
       console.log(results);
+      this.weatherTemp = results ['main']
+      this.cityName = results ['name']
+      console.log(this.weatherTemp);
+      this.weatherDetails = results['weather'][0]
+      console.log(this.weatherDetails);
+      this.weatherIcon = `https://openweathermap.org/img/wn/${this.weatherDetails.icon}@4x.png`
+
     })
 
   }
