458beb9e29a4e3b62b5ae17d9f65d3dae32c0637
Marcos-Andr4de <kinzinpc@gmail.com>
2024-05-15 13:02:56 -0300

    mobile fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_KEY: 'sua_chave_de_api_para_ambiente_de_produção',
+  API_URL: 'https://api.openweathermap.org/data/2.5/'
 };
